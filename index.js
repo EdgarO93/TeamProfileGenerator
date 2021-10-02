3959dc67d144600f41f59af0121c10bc08a82fa9
Edgar Ortega <edort93@gmail.com>
2021-10-02 17:55:56 -0500

    added some packages

---
+++ index.js
@@ -0,0 +1,6 @@
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const generatehtml = require("./utils/generateMarkdown.js");
+// An array of questions for user input
+
