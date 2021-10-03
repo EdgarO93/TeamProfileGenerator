aae7fbbd697fb08bc21df45304ad847f321f38f8
Edgar Ortega <edort93@gmail.com>
2021-10-03 00:03:03 -0500

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const employees = [];
 // const generatehtml = require("./utils/generateMarkdown.js");
 // An array of questions for user input
 
+//function too add worker
+
+//function to write to create html
