ff1d6c08a227d7e41125b65912d66148d819ef33
Edgar Ortega <edort93@gmail.com>
2021-10-02 23:54:35 -0500

    updated it to test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,32 +1,32 @@
 const Employee = require("../lib/Employee");
 
 describe('Making Employee', () => {
-    it('Making new Employee', () => {
+    test('Making new Employee', () => {
         const e = new Employee();
         expect(typeof(e)).toBe("object");
     });
 
-    it('Can make name', () => {
+    test('Can make name', () => {
         const name = "Edgar";
         const e = new Employee(name);
         expect(e.name).toBe(name);
 
     });
 
-    it("Can make id", () => {
+    test("Can make id", () => {
         const testValue = 100;
         const e = new Employee("Edgar", testValue);
         expect(e.id).toBe(testValue);
     });
 
-    it("Can make email", () => {
+    test("Can make email", () => {
         const testValue = "test@test.org";
         const e = new Employee("Edgar", 1, testValue);
         expect(e.email).toBe(testValue);
     });
 
     describe("getName", () => {
-        it("Can get name via getName()", () => {
+        test("Can get name via getName()", () => {
             const testValue = "Edgar";
             const e = new Employee(testValue);
             expect(e.getName()).toBe(testValue);
@@ -34,7 +34,7 @@ describe('Making Employee', () => {
     });
 
     describe("getId", () => {
-        it("Can get id via getId()", () => {
+        test("Can get id via getId()", () => {
             const testValue = 100;
             const e = new Employee("Edgar", testValue);
             expect(e.getId()).toBe(testValue);
@@ -42,7 +42,7 @@ describe('Making Employee', () => {
     });
 
     describe("getEmail", () => {
-        it("Can get email via getEmail()", () => {
+        test("Can get email via getEmail()", () => {
             const testValue = "test@test.org";
             const e = new Employee("Edgar", 1, testValue);
             expect(e.getEmail()).toBe(testValue);
@@ -50,7 +50,7 @@ describe('Making Employee', () => {
     });
 
     describe("getRole", () => {
-        it("getRole() should return \"Employee\"", () => {
+        test("getRole() should return \"Employee\"", () => {
             const testValue = "Employee";
             const e = new Employee("Edgar", 1, "test@test.org");
             expect(e.getRole()).toBe(testValue);
