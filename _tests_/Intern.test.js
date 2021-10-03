13c86e29f90e5f3cf33827c4c6c6eb822fc9eaf8
Edgar Ortega <edort93@gmail.com>
2021-10-02 23:48:30 -0500

    updated t test email

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,18 +2,18 @@ const Intern = require("../lib/Intern");
 
 test("Setting School", () => {
   const testValue = "AU";
-  const e = new Intern("Interny", 1, "test2@test.com", testValue);
+  const e = new Intern("Interny", 1, "test2@test.edu", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Interny", 1, "test2@test.com", "AU");
+  const e = new Intern("Interny", 1, "test2@test.edu", "AU");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
   const testValue = "AU";
-  const e = new Intern("Interny", 1, "test2@test.com", testValue);
+  const e = new Intern("Interny", 1, "test2@test.edu", testValue);
   expect(e.getSchool()).toBe(testValue);
 });
